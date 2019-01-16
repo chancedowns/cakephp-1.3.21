0c502ef0a34e4ce28b24d1a61cdee7d47bfee963
Andreas K <cakephp7@anty.at>
2019-01-16 20:22:29 +0100

    Remove debug output

--- test.php
+++
@@ -1,1 +0,0 @@
-<?php class DATABASE_CONFIG {    var $test = array('driver' => 'mysqli', 'persistent' => false, 'host' => '127.0.0.1', 'login' => 'travis', 'password' => '', 'database' => 'test', 'prefix' => '',);}
