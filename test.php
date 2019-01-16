1ab29e09f9e8df72b91a267e4f0c0a05fa386c0c
Andreas K <cakephp7@anty.at>
2019-01-16 20:11:21 +0100

    Fix travis database-file

---
+++ test.php
@@ -0,0 +1,1 @@
+<?php class DATABASE_CONFIG {    var $test = array('driver' => 'mysqli', 'persistent' => false, 'host' => '127.0.0.1', 'login' => 'travis', 'password' => '', 'database' => 'test', 'prefix' => '',);}
